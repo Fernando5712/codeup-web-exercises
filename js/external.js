1f15022651d499c4ced714c1692d2f85ff0930a2
Fernando Hernandez <Fernando.Hernandez.Fabars@gmail.com>
2020-04-20 17:04:11 -0500

    functions.js

--- js/external.js
+++ js/external.js
@@ -5,10 +5,23 @@ alert("Great " + favColor + "  is my favorite color too")
 
 var rentalDays = prompt("How many days total have you rented")
 var moviePrice = prompt("How much money per day")
-alert("Your total amount for the movies is " + rentalDays * moviePrice)
+alert("Your total amount for the movies is " + parseFloat(rentalDays) * parseFloat(moviePrice))
 
 var companies = prompt("How many companies are you employed by")
 alert(companies + " ! WOW, Staying busy")
 var ammount = prompt("How much did each company pay you total , please dont use commas")
 var hours = prompt("How many hours did you work total")
 alert("your total income for the week is " + ammount * hours)
+
+function returnFive() {
+    return 5;
+}
+function isShortWord(str) {
+    return str.length < 5;
+}
+function isSameLength(str1, str2) {
+    return str1.length === str2.length;
+}
+function getSmallerSegment(str, length) {
+    return str.substring(0, length).toLowerCase();
+}
