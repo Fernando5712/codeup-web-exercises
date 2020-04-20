2bf0b6d30d7a3f86f249d1a0d4016420030374b7
Fernando Hernandez <Fernando.Hernandez.Fabars@gmail.com>
2020-04-20 10:15:12 -0500

    js with html

---
+++ js/external.js
@@ -0,0 +1,14 @@
+console.log(" Hello From External JS")
+alert('Welcome to my website')
+var favColor = prompt("What is your favorite color")
+alert("Great " + favColor + "  is my favorite color too")
+
+var rentalDays = prompt("How many days total have you rented")
+var moviePrice = prompt("How much money per day")
+alert("Your total amount for the movies is " + rentalDays * moviePrice)
+
+var companies = prompt("How many companies are you employed by")
+alert(companies + " ! WOW, Staying busy")
+var ammount = prompt("How much did each company pay you total , please dont use commas")
+var hours = prompt("How many hours did you work total")
+alert("your total income for the week is " + ammount * hours)
