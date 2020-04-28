d39185be5dc88c3c8569b277a2194f829274833b
Fernando Hernandez <Fernando.Hernandez.Fabars@gmail.com>
2020-04-28 16:07:37 -0500

    loops.js

---
+++ js/for_loops.js
@@ -0,0 +1,27 @@
+// function showMultiplicationTable() {
+//     var base = prompt("choose a number", "");
+//     var num = 1;
+//     for (num; num <= 10; ++num)
+//         console.log(base  + " * " + num + " = " + base * num);
+// }
+// console.log(showMultiplicationTable());
+
+
+// for (var num = 1; num <= 10; num++) {
+//     var x = Math.floor(Math.random() * 180) + 20;
+//     if (x % 2 ===0){
+//     console.log(x + " is even")}
+//     else console.log(x + " is odd")
+//
+// }
+
+
+// for( i = 1; i <= 9; i++ ) {
+//     var x = "".repeat(9 - i) + (i + "").repeat(i);
+//     console.log(x)
+// }
+
+for (var i = 100; i >= 5; i -= 5){
+    console.log(i)
+}
+
